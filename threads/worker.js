1796e3755a03d1ecd7540534d99952882c4637bd
michelle bautista <alejandra.lopez.is@unipolidgo.edu.mx>
2021-10-27 17:33:56 -0500

    '(marca)'

---
+++ threads/worker.js
@@ -0,0 +1,29 @@
+let ffmpeg = require('ffmpeg');
+const fs = require('fs');
+
+const { workerData, parentPort } = require('worker_threads');
+let dest = '/dest/video.mp4';
+
+try {
+    
+    let process = new ffmpeg(workerData.file);
+    process.then(video => {
+        video.fnAddWatermark(
+            __dirname + '/marcadeagua.png',
+            __dirname + '/' + workerData.filename,
+            {position: 'C'}
+        ),
+        (err, file) => {
+            if(!err){
+                console.log('El nombre del video procesado es '+ file);
+                parentPort.postMessage({ status: 'Done', file: file });
+            }else{
+                console.log(err);
+            }
+        }
+    });
+
+} catch (error) {
+    console.log(error.code);
+    console.log(error.msg);
+}
