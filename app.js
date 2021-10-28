9d3df818bff3bc8174f0c78936648239530f7cfa
michelle bautista <alejandra.lopez.is@unipolidgo.edu.mx>
2021-10-27 20:53:06 -0500

    corregido

--- app.js
+++ app.js
@@ -2,58 +2,59 @@ import express from 'express';
 import * as bodyParser from 'body-parser';
 import * as path from 'path';
 import multer from 'multer';
-import { Worker, isMainThread, workerData } from 'worker_threads';
+import{Worker,isMainThread,workerData} from'worker_threads';
 
-require('dotenv').config();
+require("dotenv").config();
 
 const storage = multer.diskStorage({
     destination: './uploads/',
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 });
 
-const upload = multer({dest: 'uploads', storage: storage});
+const upload = multer({ dest: 'uploads', storage: storage});
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended:false }));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
-app.post('/upload-video', upload.single('ssvideo'), (req, res) => {
-    if(isMainThread){
+app.post("/upload-video", upload.single("ssvideo"), (req, res) => {
+    if(isMainThread) {
 
-        let thread = new Worker('./threads/worker.js', { 
+        let thread = new Worker("./threads/worker.js", {
             workerData: {
                 file: req.file.path,
                 filename: req.file.filename
-            } 
+            }
         });
 
-        thread.on('message', data => {
+        thread.on("message", (data) => {
             res.download(data.file, req.file.filename);
         });
 
-        thread.on('error', err => {
-            console.log('Error en el thread', err);
+        thread.on("error", (err) => {
+            console.log("Error en el thread", err);
         });
 
-        thread.on('exit', code =>{
-            if(code != 0){
-                console.log(`El hilo se detuvo con el codigo de salida ${code}`);
+        thread.on("exit", (code) => {
+            if(code !=0) {
+                console(`El hilo se detuvo con el codigo de salida: ${code}`);
             }
-        });
-    }
+        })
 
+    }
+    
 });
 
-const PORT = 8080;
-app.listen(PORT, () => {
-    console.log(`El servidor se inicio en el puerto ${PORT}`);
+const PORT = 3000;
+app.listen(PORT, ()=> {
+    console.log(`El servidor inicio por el puerto ${PORT}`);
 });
--- threads/worker.js
+++ threads/worker.js
@@ -1,29 +1,33 @@
-let ffmpeg = require('ffmpeg');
-const fs = require('fs');
+let ffmpeg = require("ffmpeg");
+const fs = require("fs");
 
-const { workerData, parentPort } = require('worker_threads');
-let dest = '/dest/video.mp4';
+const {workerData, parentPort} = require("worker_threads");
+let dest = "/dest/video.mp4";
 
 try {
-    
+
     let process = new ffmpeg(workerData.file);
     process.then(video => {
         video.fnAddWatermark(
-            __dirname + '/marcadeagua.png',
-            __dirname + '/' + workerData.filename,
-            {position: 'C'}
-        ),
-        (err, file) => {
-            if(!err){
-                console.log('El nombre del video procesado es '+ file);
-                parentPort.postMessage({ status: 'Done', file: file });
-            }else{
-                console.log(err);
+            __dirname + "/marcadeagua.png",
+            __dirname + "/" + workerData.filename,
+            {
+                position: "C"
+            },
+            function(err, file) {
+                if(!err) {
+                    console.log("El nombre del video procesado es" + file);
+                    parentPort.postMessage({ status: "Done", file:file });
+                }
+                else {
+                    console.log(err);
+                }
             }
-        }
+        )
     });
 
-} catch (error) {
-    console.log(error.code);
-    console.log(error.msg);
+}
+catch(e) {
+    console.log(e.code);
+    console.log(e.msg);
 }
